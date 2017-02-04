384eb51d516318e4a120a805457d9cb3c3062cad
Zachary Hoffman <zachary.j.hoffman@gmail.com>
2017-02-04 18:20:30 -0500

    basic timed server requests

--- index.js
+++ index.js
@@ -1,63 +1,50 @@
-var express = require( 'express' );
-// body parsing middleware
+var express = require('express'), json = require('express-json');
 var bodyParser = require('body-parser');
-//twilio for text!
 var twilio = require('twilio');
+var schedule = require('node-schedule');
+var fs = require("fs");
+var http = require('http');
 
-var app = express(); // creates an instance of an express application.
-
-//reads the secret twilio file
- var fs = require("fs");
- var secretContents = fs.readFileSync("twilio-secret.json");
-
+var app = express().use(json()); // creates an instance of an express application.
 // Find your account sid and auth token in your Twilio account Console.
-var client = twilio(secretContents.pid, secretContents.auth);
+var client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
-client.messages.create({
-    to: "+14159397520",
-    from: "+14156836411",
-    body: "This is the ship that made the Kessel Run in fourteen parsecs?",
-}, function(err, message) {
-    console.log(message.sid);
+
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+app.post('/phone', function(req, res) {
+  var number = req.body.number,
+      message = req.body.message,
+      time = req.body.time;
+  console.log("number: " + number + "\n" + "message: " + message + "\n" + "time: " + time)
+  var scheduler = schedule.scheduleJob(time + " *", function() {
+    client.messages.create({
+        to: "+1" + number,
+        from: "+14156836411",
+        body: message,
+    }, function(err, message) {
+        console.log(message.sid);
+    });
+  });
 });
 
-// body parsing middleware
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true })); // for HTML form submits
-app.use(bodyParser.json()); // would be for AJAX requests
-
-var someTweets = [{id: '1', name:'Jason', content:'At Hack@Brown 2017'}, {id: '2', name:'Obama', content:'Hello there'},
-{id: '3', name:'Trump', content:'It is going to be big and spectacular'}, {id: '4', name:'Alexandra', content:'I love cats <3'}]
-
-
-var data = {};
+app.get('/',function(req,res){
+     res.sendFile(__dirname + '/index.html');
+});
 
 app.listen('3000', function(){
   console.log("Server running");
 });
 
-app.get("/", function(req, res){
-  res.send("/tweetId to see tweets");
-});
-
-app.get("/:tweetId", function(req, res){
-  res.send(data[req.params.tweetId]);
-});
-
-app.post('/', function(req, res){
-    var newTweet = req.body;
-    addTweet(newTweet);
-    res.send('Tweet successfully added!');
-})
-
-function addTweet(tweet){
-    data[tweet.id] = tweet;
-}
-
-function removeTweet(tweetId){
-    data[tweetId] = undefined;
-}
-
-for(var i = 0; i < someTweets.length; i++){
-    addTweet(someTweets[i]);
-}
+/*
+var scheduler = schedule.scheduleJob('10 43 17 4 2 *', function(){
+  client.messages.create({
+      to: "+14159397520",
+      from: "+14156836411",
+      body: "Is it 5:43?",
+  }, function(err, message) {
+      console.log(message.sid);
+  });
+});*/
