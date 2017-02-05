6c2450c1579a7b52365f485e53963ccbbf7def52
Zachary Hoffman <zachary.j.hoffman@gmail.com>
2017-02-04 21:16:04 -0500

    date sorting

--- parser.js
+++ parser.js
@@ -1,11 +1,24 @@
 module.exports = {
-  parseDate: function () {
-    // whatever
+  parseDate: function (date, time) {
+    var dateArray = date.split("-");
+    return removeZero(dateArray[1]) + " " + removeZero(dateArray[2]);
+  },
+  parseTime: function (time) {
+    var dateArray = time.split(":");
+    return removeZero(dateArray[1]) + " " + removeZero(dateArray[0]);
   },
   parseNumber: function () {
     // whatever
   },
-  parseTime: function () {
-    // whatever
+  parseDateSortable: function(date){
+
   }
 };
+
+function removeZero(date){
+  if (date.charAt(0) == 0){
+    return date.charAt(1);
+  } else {
+    return date;
+  }
+}
