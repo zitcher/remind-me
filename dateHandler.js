f9161714c8f38adb06a035b0d5920587b7878c9f
Zachary Hoffman <zachary.j.hoffman@gmail.com>
2017-02-04 23:56:48 -0500

    data structure built

--- dateHandler.js
+++ dateHandler.js
@@ -4,4 +4,30 @@ module.exports = {
     if (date1 < date2) return -1;
     return 0;
   },
+  addDate: function(array, date, start, end){
+    while(true) {
+      curPos = Math.floor((start + end) / 2);
+      console.log("start" + start);
+      console.log("end" + end);
+      curDate = array[curPos];
+      if (start >= end) {
+        if (curDate === undefined) {
+          array.splice(curPos, 0, date);
+          return;
+        } else if (curDate.date > date.date) {
+          array.splice(curPos, 0, date);
+          return;
+        } else {
+          array.splice(curPos + 1, 0, date);
+          return;
+        }
+      } else {
+        if (date.date > curDate.date) {
+          start = curPos + 1;
+        } else {
+          end = curPos;
+        }
+      }
+    }
+  }
 };
